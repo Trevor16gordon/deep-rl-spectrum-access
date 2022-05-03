1c3ea4c105bcc54aaf73fc2da5505c3b1770e879
trevor16gordon <trevor16gordon@gmail.com>
2022-05-03 10:42:50 -0400

    Cleaning up main train file

---
+++ config_file.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Single place for all config"""
+
+
+class TrainingConfig():
+    """
+    Config for training Info GAN
+    """
+    EPS_DECAY = 1e-5
+    EPSILON = 0.1
+    NUM_AGENTS = 2
+    NUM_BANDS = 1
+    TEMPORAL_LENGTH = 5
+    REWARD_TYPE = "collisionpenality2"
+    OBS_TYPE = "aggregate3"
+    AGENTS_SHARED_MEMORY = True
+    BUFFER_SIZE = 1000
+    EPISODE_LEN = 40001
+    TEMPERATURE = 0.005
+    REWARD_HISTORY_LEN = 100
+    MODEL_TYPE = "ddqn"
+    AGENT_HOMOGENEITY = "all_same" # Can also be "one_periodic"
+
+
+class ReportingConfig():
+    """
+    Config for reporting / visulalization
+    """
+    TOP_LEVEL_FOLDER = "plots.nosync/"
+    IMAGES_TO_SAVE = 10
+    SAVE_CHECKPOINT_CSV_EVERY = 500
+    PRINT_UPDATE_EVERY = 10
+    CONFIG_FILE_NAME_TO_SAVE = "config.csv"
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from multiprocessing import shared_memory
-import tensorflow as tf
-import numpy as np
-import pandas as pd
 import os
 import pdb
 import tqdm
+import argparse
+import tensorflow as tf
+import numpy as np
+import pandas as pd
 from datetime import datetime
 from environment import FrequencySpectrumEnv
-from agent import DynamicSpectrumAccessAgent1, DynamicSpectrumAccessAgentPeriodic#, DynamicSpectrumAccessAgent2
-from utils import  agent_actions_to_information_table
-from visualization import plot_spectrum_usage_over_time
-from utils import create_stacked_csv, complete_df_to_stacked, create_stacked_csv_old
-import argparse
+from agent import (DynamicSpectrumAccessAgent1,
+                   DynamicSpectrumAccessAgentActorCritic,
+                   DynamicSpectrumAccessAgentPeriodic)
+from utils import agent_actions_to_information_table, create_stacked_csv
+from config_file import TrainingConfig, ReportingConfig
+
+
+tf.compat.v1.enable_eager_execution()
 
 # Disable all GPUS
 tf.config.set_visible_devices([], 'GPU')
@@ -20,108 +23,135 @@ for device in visible_devices:
     assert device.device_type != 'GPU'
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--eps_decay", "-epsd", default=1e-5, type=float)
-parser.add_argument("--epsilon", "-eps", default=0.1, type=float)
-parser.add_argument("--num_agents", "-na", default=2, type=int)
-parser.add_argument("--num_bands", "-b", default=1, type=int)
-parser.add_argument("--temporal_length", "-tl", default=5, type=int)
-parser.add_argument("--reward_type", "-r", default="collisionpenality2")
-parser.add_argument("--obs_type", "-o", default="aggregate3")
-parser.add_argument("--agents_shared_memory", "-sm", default=1)
-parser.add_argument("--buffer_size", "-bs", default=1000, type=int)
-parser.add_argument("--episode_len", "-el", default=40001, type=int)
-parser.add_argument("--temperature", "-t", default=0.005, type=float)
-parser.add_argument("--reward_history_len", "-rhl", default=100, type=int)
+def train(args):
 
-args = parser.parse_args()
-all_config = parser.parse_args().__dict__
+    all_config = args.__dict__
+
+    if all_config["obs_type"] == "own_actions":
+        all_config["obvs_space_dim"] = args.num_bands+2
+    elif all_config["obs_type"] == "aggregate":
+        all_config["obvs_space_dim"] = args.num_bands
+    elif all_config["obs_type"] == "aggregate2":
+        all_config["obvs_space_dim"] = 2*args.num_bands+2
+    elif all_config["obs_type"] == "aggregate3":
+        all_config["obvs_space_dim"] = 2*args.num_bands+2
+
+    all_config["time_folder"] = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+    path = ReportingConfig.TOP_LEVEL_FOLDER + all_config["time_folder"]
+    os.mkdir(path)
+
+    df_config = pd.DataFrame.from_dict([all_config])
+    df_config.to_csv(os.path.join(
+        path, ReportingConfig.CONFIG_FILE_NAME_TO_SAVE))
+
+    env = FrequencySpectrumEnv(
+        args.num_bands,
+        args.num_agents,
+        temporal_len=args.temporal_length,
+        reward_history_len=args.reward_history_len,
+        reward_type=args.reward_type,
+        observation_type=args.obs_type)
+
+    if args.model_type == "ddqn":
+        agents = [DynamicSpectrumAccessAgent1(args.num_bands,
+                                              args.obvs_space_dim,
+                                              temporal_length=args.temporal_length,
+                                              epsilon=args.epsilon,
+                                              temperature=args.temperature,
+                                              buffer_size=args.buffer_size,
+                                              epsilon_decay=args.eps_decay) for _ in range(args.num_agents)]
+    else:
+        agents = [DynamicSpectrumAccessAgentActorCritic(args.num_bands,
+                                                        args.obvs_space_dim,
+                                                        temporal_length=args.temporal_length,
+                                                        epsilon=args.epsilon,
+                                                        epsilon_decay=args.eps_decay) for _ in range(args.num_agents)]
+
+    if args.agent_homogeneity == "one_periodic":
+        agents[-1] = DynamicSpectrumAccessAgentPeriodic(1, 1, 1)
+
+    if args.agents_shared_memory:
+        for agent_i in agents[1:]:
+            agent_i.memory = agents[0].memory
+
+    state = env.reset()
+
+    total_reward = 0
+    counter = 0
+    agent_values = []
+    agent_action_prob = []
+
+    for counter in (pbar := tqdm.tqdm(range(args.episode_len))):
+        # counter += 1
+
+        if counter > int(all_config["episode_len"]):
+            print("Breaking because episode is done")
+            break
+
+        actions = [agents[i].act(state[i]) for i in range(args.num_agents)]
+
+        # Collect agent value neural networks and resulting probabilities for analysis
+        agent_values.append(
+            [agents[i].last_value_function for i in range(args.num_agents)])
+        agent_action_prob.append(
+            [agents[i].last_prob_value for i in range(args.num_agents)])
+
+        next_state, rewards, done_i, info = env.step(actions)
+
+        for i in range(args.num_agents):
+            agents[i].observe_result(
+                state[i], actions[i], rewards[i], next_state[i], done_i[i])
+
+        state = next_state
+        total_reward += sum(rewards)
+
+        if (counter % ReportingConfig.PRINT_UPDATE_EVERY) == 0:
+            pbar.set_description(
+                (f"Episodes: {counter}  Total reward: {total_reward} Throughput: {env.throughput:.2f}"
+                f" Collisions: {env.num_collisions} Epsilon: {agents[0].epsilon:.2f}"))
+
+        if (counter % ReportingConfig.SAVE_CHECKPOINT_CSV_EVERY) == 0:
+            agent_actions = np.array(env.agent_actions_complete_history)
+            df_more_info = agent_actions_to_information_table(
+                agent_actions, reward_type=all_config["reward_type"])
+            df = create_stacked_csv(
+                agent_actions, agent_values, agent_action_prob)
+            merged = pd.merge(df_more_info, df, on="time")
+            merged.to_csv(f"{path}/history.csv")
 
 
-num_bands = int(all_config["num_bands"])
-num_agents = int(all_config["num_agents"])
+if __name__ == "__main__":
 
-if all_config["obs_type"] == "own_actions":
-    all_config["obvs_space_dim"] = num_bands+2
-elif all_config["obs_type"] == "aggregate":
-    all_config["obvs_space_dim"] = num_bands
-elif all_config["obs_type"] == "aggregate2":
-    all_config["obvs_space_dim"] = 2*num_bands+2
-elif all_config["obs_type"] == "aggregate3":
-    all_config["obvs_space_dim"] = 2*num_bands+2
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eps_decay", "-epsd",
+                        default=TrainingConfig.EPS_DECAY, type=float)
+    parser.add_argument("--epsilon", "-eps",
+                        default=TrainingConfig.EPSILON, type=float)
+    parser.add_argument("--num_agents", "-na",
+                        default=TrainingConfig.NUM_AGENTS, type=int)
+    parser.add_argument("--num_bands", "-b",
+                        default=TrainingConfig.NUM_BANDS, type=int)
+    parser.add_argument("--temporal_length", "-tl",
+                        default=TrainingConfig.TEMPORAL_LENGTH, type=int)
+    parser.add_argument("--reward_type", "-r",
+                        default=TrainingConfig.REWARD_TYPE, type=str)
+    parser.add_argument("--obs_type", "-o",
+                        default=TrainingConfig.OBS_TYPE, type=str)
+    parser.add_argument("--agents_shared_memory", "-sm",
+                        default=TrainingConfig.AGENTS_SHARED_MEMORY, type=int)
+    parser.add_argument("--buffer_size", "-bs",
+                        default=TrainingConfig.BUFFER_SIZE, type=int)
+    parser.add_argument("--episode_len", "-el",
+                        default=TrainingConfig.EPISODE_LEN, type=int)
+    parser.add_argument("--temperature", "-t",
+                        default=TrainingConfig.TEMPERATURE, type=float)
+    parser.add_argument("--reward_history_len", "-rhl",
+                        default=TrainingConfig.REWARD_HISTORY_LEN, type=int)
+    parser.add_argument("--model_type", "-mt",
+                        default=TrainingConfig.MODEL_TYPE, type=str)
+    parser.add_argument("--agent_homogeneity", "-ah",
+                        default=TrainingConfig.AGENT_HOMOGENEITY, type=str)
 
-top_level_folder = "plots.nosync/"
-all_config["time_folder"] = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
-path = top_level_folder + all_config["time_folder"]
-os.mkdir(path)
+    args = parser.parse_args()
 
-# all_config["agent_homogeneity"] = "one_periodic"
-all_config["agent_homogeneity"] = "all_same"
-
-pd.DataFrame.from_dict([all_config]).to_csv(path + "/config.csv")
-
-env = FrequencySpectrumEnv(
-    num_bands,
-    num_agents, 
-    temporal_len=int(all_config["temporal_length"]),
-    reward_history_len = args.reward_history_len,
-    reward_type=all_config["reward_type"], 
-    observation_type=all_config["obs_type"])
-
-agents = [DynamicSpectrumAccessAgent1(num_bands,
-                                        all_config["obvs_space_dim"], 
-                                        temporal_length=int(all_config["temporal_length"]), 
-                                        epsilon=float(all_config["epsilon"]),
-                                        temperature=float(all_config["temperature"]),
-                                        buffer_size=int(all_config["buffer_size"]),
-                                         epsilon_decay=float(all_config["eps_decay"])) for _ in range(num_agents)]
-
-if all_config["agent_homogeneity"] == "one_periodic":
-    agents[-1] = DynamicSpectrumAccessAgentPeriodic(1, 1, 1)
-
-
-if all_config["agents_shared_memory"]:
-    for agent_i in agents[1:]:
-        agent_i.memory = agents[0].memory
-
-save_checkpoint_csv_every = 500
-info_every = 10
-
-state = env.reset()
-
-
-total_reward = 0
-counter = 0
-agent_values = []
-agent_action_prob = []
-
-for counter in tqdm.tqdm(range(args.episode_len)):
-    # counter += 1
-
-    if counter > int(all_config["episode_len"]):
-        print("Breaking because episode is done")
-        break
-
-    actions = [agents[i].act(state[i]) for i in range(num_agents)]
-
-    # Collect agent value neural networks and resulting probabilities for analysis
-    agent_values.append([agents[i].last_value_function for i in range(num_agents)])
-    agent_action_prob.append([agents[i].last_prob_value for i in range(num_agents)])
-
-    next_state, rewards, done_i, info = env.step(actions)
-
-    for i in range(num_agents):
-        agents[i].observe_result(state[i], actions[i], rewards[i], next_state[i], done_i[i])
-        
-    state = next_state
-    total_reward += sum(rewards)
-
-    if (counter % info_every) == 0:
-        print(f"total reward after {counter} episode is {total_reward} throughput is {env.throughput:.2f} num_collisions is {env.num_collisions} and epsilon is {agents[0].epsilon:.2f}")
-    
-    if (counter % save_checkpoint_csv_every) == 0:
-        agent_actions = np.array(env.agent_actions_complete_history)
-        df_more_info = agent_actions_to_information_table(agent_actions, reward_type=all_config["reward_type"])
-        df = create_stacked_csv(agent_actions, agent_values, agent_action_prob)
-        merged= pd.merge(df_more_info, df, on="time")
-        merged.to_csv(f"{path}/history.csv")
+    train(args)
